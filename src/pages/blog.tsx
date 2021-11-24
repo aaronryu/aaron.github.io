3be7ba88a13af90d323962ff8b243c37ae28a91d
Aaron Ryu <fbcndah@gmail.com>
2021-11-24 23:02:51 +0900

    [B-000] init: Link provided by gastby

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -2,23 +2,23 @@ import * as React from 'react'
 import Layout from "../component/layout"
 import { graphql } from 'gatsby'
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import { Link } from 'gatsby'
 
 export default function BlogPage({ data }) {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
@@ -39,6 +39,7 @@ export const query = graphql`
             modifiedTime(formatString: "MMMM D, YYYY")
           }
         }
+        slug
       }
     }
   }
